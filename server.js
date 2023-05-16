7886e780a7e2ba90a09a77a3e1138a630efd2088
seonwooke <seonookkim@gmail.com>
2023-05-16 17:15:27 +0900

    :construction: HTTP Routing
    HTTP Routing

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const http = require('http');
 const port = 4000;
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json'
-  });
-  res.end(
-    JSON.stringify({
-      a: "a",
-      b: "b"
-    }
-  ));
+  if (req.url === '/home') {
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    res.end(
+      JSON.stringify({
+        a: "a",
+        b: "b"
+      }
+    ));
+  } else if (req.url === '/about') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<h1>About Page</h1>');
+    res.write('</body>');
+    res.write('</html>');
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(port, () => {
