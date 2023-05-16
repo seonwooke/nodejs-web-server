60df27f0888e796f2c7f9e2668b108503f9133f1
seonwooke <seonookkim@gmail.com>
2023-05-16 17:11:07 +0900

    :construction: Create Server
    Create Server

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const port = 4000;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  res.end(
+    JSON.stringify({
+      a: "a",
+      b: "b"
+    }
+  ));
+});
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
